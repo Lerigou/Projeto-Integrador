e9b8671e6a7dea6ac178b8b93fc7625216b1eb46
blackkseaw <blackkseaw@gmail.com>
2022-11-21 20:54:58 -0300

    Arrumando query insert de pesquisa

---
+++ BddConection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BddConection {
+
+    private static final  String URL = "jdbc:mysql://localhost:3306/pi";
+    private static final String USER = "root";
+    //private static final String SENHA = "root";
+    private static final String SENHA = "1234";
+
+    private static Connection conexao;
+    private BddConection() {}
+
+    public static Connection getConexao() throws SQLException {
+        if (conexao == null) {
+            conexao = DriverManager.getConnection(URL, USER, SENHA);
+        }
+        return conexao;
+    }
+
+} // fim da classe BddConection
